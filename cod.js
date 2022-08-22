4e1f2ad93c0e50f71aef5c42d0c340f7a290d082
elijah obara <elijahobara357@gmail.com>
2022-08-22 20:00:58 +0300

    sudoku challenge leetcode

--- cod.js
+++ cod.js
@@ -186,3 +186,74 @@ var isPalindrome =(head) => {
     const tail = reverse(midpoint);
     return compare(head, tail);
 }
+
+//determine if a 9 x 9 sudoku board is valid .only the filled cells need to be validated according to the rules : each row must contain the digits 1-9 without repetition.Each column must contain the digits 1-9 without repetition
+//each of the 3 x 3 sub-boxes of the grid must contain the digits 1-9 wuthout repetition
+
+var isValidSudoku = function(board) {
+
+  for(let i = 0; i < board.length; i++ ) {
+    const set = new Set();
+    for(let j = 0; j < board[i].length; j++){
+      const cell = board[i][j];
+      if(cell === '.') continue
+      if(set.has(cell)) return false;
+      set.add(cell);
+    }   
+  }
+
+  for(let i = 0; i < board.length; i++ ) {
+    const set = new Set();
+    for(let j = 0; j < board[i].length; j++){
+      const cell = board[j][i];
+      if(cell === '.') continue
+      if(set.has(cell)) return false;
+      set.add(cell);
+    }   
+  }
+
+  for(let i =0; i< 3; i++ ) {
+    for(let j =0; j < 3; j++) {
+      const set = new Set();
+      for(let  k = 0; k < 3; k++) {
+        for(let l = 0; l < 3; l++) {
+          const cell = board[3 * i + k][3 * j + l];
+          if(cell === '.') continue
+          if(set.has(cell)) return false;
+          set.add(cell);
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+// method 2
+
+
+var isValidSudoku = function(board) {
+
+  const rows = [], cols = [], boxes = [];
+  for(let i = 0; i < board.length; i++) {
+    rows.push(new Set());
+    cols.push(new Set());
+    boxes.push(new Set());
+  }
+
+  for(let i = 0; i < board.length; i++ ) {
+    for(let j = 0; j < board[i].length; j++){
+      if(cell === '.') continue;
+      const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+      if(rows[i].has(cell) || cols[j].has(cell) || boxes[boxNum].has(cell))
+       return false;
+      rows[i].add(cell);
+      cols[j].add(cell);
+      boxes[boxNum].add(cell);
+    }
+  }
+  
+  return true;
+}
+
+//method 3
